172a2f003c74fd369c270f3dc85624d1b1f324ba
Md Kaif Sardar <147906935+MdKaifSardar@users.noreply.github.com>
2024-07-03 17:33:04 +0530

    changed the proxy

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://skillsync-api-deployment.vercel.app'
-      // '/api': 'http://localhost:5000'
+      '/api': 'https://skillsync-api-deployment.vercel.app/'
     }
   },
   plugins: [react()],
