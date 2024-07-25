fd8b47349dd64bba9fe6cb8ec6719196d3e15e8b
Md Kaif Sardar <mkaifsard564773@gmail.com>
2024-07-25 16:01:40 +0530

    new commit

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,7 @@ const about = () => {
   return (
     <div className='h-screen flex flex-col justify-center items-center mt-28 md:p-10 p-2 gap-3'>
       <span className='md:text-4xl text-2xl font-sans font-bold blue-gradient_text'>About The Project</span>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, sint.</div>
+      <div>hehe Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, sint.</div>
     </div>
   )
 }
